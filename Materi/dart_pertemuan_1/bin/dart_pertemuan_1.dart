684709e4668eabb45a65e4dc991bee51e18b4f7c
pickle40 <tanotodavidriski@yahoo.co.id>
2022-03-09 17:18:18 +0700

    materi

--- Materi/dart_pertemuan_1/bin/dart_pertemuan_1.dart
+++ Materi/dart_pertemuan_1/bin/dart_pertemuan_1.dart
@@ -1,84 +1,84 @@
-// import 'dart:io';
+import 'dart:io';
 
-// //Parameter Function
-// String hitung(String nama, {int? uang, int? pengeluaran}){
-//   int sisauang = uang! - pengeluaran!;
-//   return "$nama sisa uangnya sebesar $sisauang";
-// }
+//Parameter Function
+String hitung(String nama, {int? uang, int? pengeluaran}){
+  int sisauang = uang! - pengeluaran!;
+  return "$nama sisa uangnya sebesar $sisauang";
+}
 
-// //Parameter Function Positional
-// String hitungpositional(String nama, [int? uang, int? pengeluaran]){
-//   int sisauang = uang! - pengeluaran!;
-//   return "$nama sisa uangnya sebesar $sisauang";
-// }
+//Parameter Function Positional
+String hitungpositional(String nama, [int? uang, int? pengeluaran]){
+  int sisauang = uang! - pengeluaran!;
+  return "$nama sisa uangnya sebesar $sisauang";
+}
 
-// //DEMO : Higher Order
-// void fungsiHOrder(String nama, Function funcparam){
-//   print(nama + " " + funcparam(3,5).toString());
-// }
+//DEMO : Higher Order
+void fungsiHOrder(String nama, Function funcparam){
+  print(nama + " " + funcparam(3,5).toString());
+}
 
-// //DEMO : Closures
-// Function terimaData(data){
-//   return() => print("Data yang dikirim $data");
-// }
+//DEMO : Closures
+Function terimaData(data){
+  return() => print("Data yang dikirim $data");
+}
 
-// void main(List<String> arguments) {
-//   //DEMO : Closures - MAIN
-//   //Variable yang berisi fungsi
-//   var fmain = terimaData("abc");
-//   fmain();
+void main(List<String> arguments) {
+  //DEMO : Closures - MAIN
+  //Variable yang berisi fungsi
+  var fmain = terimaData("abc");
+  fmain();
 
-//   //DEMO : HIGHER ORDER - MAIN
-//   fungsiHOrder("F. Tambah", (a, b) => a+b);
-//   fungsiHOrder("F. Kali", (a, b) => a*b);
+  //DEMO : HIGHER ORDER - MAIN
+  fungsiHOrder("F. Tambah", (a, b) => a+b);
+  fungsiHOrder("F. Kali", (a, b) => a*b);
 
-//   //DEMO : First Class
-//   var total = (int bil1, int bil2){
-//     return bil1 + bil2;
-//   };
-//   print(total(5,5));
+  //DEMO : First Class
+  var total = (int bil1, int bil2){
+    return bil1 + bil2;
+  };
+  print(total(5,5));
 
-//   //DEMO : Lambda Function
-//   var totallambda = (int bil1, int bil2) => bil1 + bil2;
-//   print(totallambda(5,5));
+  //DEMO : Lambda Function
+  var totallambda = (int bil1, int bil2) => bil1 + bil2;
+  print(totallambda(5,5));
 
-//   //DEMO : Function Parameter
-//   print(hitung("David", uang: 100000, pengeluaran: 20000));
-//   print(hitungpositional("Merkurius", 100000, 50000));
+  //DEMO : Function Parameter
+  print(hitung("David", uang: 100000, pengeluaran: 20000));
+  print(hitungpositional("Merkurius", 100000, 50000));
 
-//   //DEMO : INPUT OUTPUT
-//   stdout.write("Masukkan kata : ");
-//   String? input = stdin.readLineSync();
-//   int angka = int.parse(input!);
+  //DEMO : INPUT OUTPUT
+  stdout.write("Masukkan kata : ");
+  String? input = stdin.readLineSync();
+  int angka = int.parse(input!);
 
-//   print(input);
+  print(input);
 
-//   //DEMO : IF ELSE
-//   if(angka % 2 == 1){
-//     print("GANJIL");
-//   }
-//   else{
-//     print("GENAP");
-//   }
+  //DEMO : IF ELSE
+  if(angka % 2 == 1){
+    print("GANJIL");
+  }
+  else{
+    print("GENAP");
+  }
 
-//   //DEMO : SWITCH CASE
-//   switch(angka % 2){
-//     case 1:
-//       print("GANJIL SW");
-//       break;
-//     case 0:
-//       print("GENAP SW");
-//       break;
-//   }
+  //DEMO : SWITCH CASE
+  switch(angka % 2){
+    case 1:
+      print("GANJIL SW");
+      break;
+    case 0:
+      print("GENAP SW");
+      break;
+  }
 
-//   //DEMO : FOR
-//   for(int i=0; i<angka; i++){
-//     print("angka: $i");
-//   }
+  //DEMO : FOR
+  for(int i=0; i<angka; i++){
+    print("angka: $i");
+  }
 
-//   //DEMO : DO WHILE
-//   while(angka > 0){
-//     print(angka);
-//     angka--;
-//   }
-// }
+  //DEMO : DO WHILE
+  while(angka > 0){
+    print(angka);
+    angka--;
+  }
+}

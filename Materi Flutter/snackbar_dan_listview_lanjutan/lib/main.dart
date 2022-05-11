7863b717d9b064efe51d45c3af1f0e2088fb147d
pickle40 <tanotodavidriski@yahoo.co.id>
2022-05-11 18:22:47 +0700

    ListView advance dan SnackBar

--- Materi Flutter/snackbar_dan_listview_lanjutan/lib/main.dart
+++ Materi Flutter/snackbar_dan_listview_lanjutan/lib/main.dart
@@ -35,6 +35,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Coba Widget",
       home: Scaffold(
         appBar: AppBar(title: Text("Coba Widget")),
